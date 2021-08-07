f390959fc419cb7863fcbf46e609b1bd11a574a1
Visitor <Visitor@THINKSTATION>
2021-08-07 16:12:59 -0400

    Updated the directions on how to build the executable Tomcat JAR file.

--- numberguesser/src/main/java/com/mcnz/servlet/NumberGuesserGame.java
+++ numberguesser/src/main/java/com/mcnz/servlet/NumberGuesserGame.java
@@ -8,19 +8,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+
 To build an executable JAR file, run the following command in the same folder as the POM:
 
 mvn clean install tomcat7:exec-war-only
 
 NOTE: Before you run the executable jar, make sure Jenkins isn't running on port 8080. 
+
 Also, make sure any other Tomcat instances you have started are stopped.
+
 If port 8080 is in use, the JAR file will not start.
 
 To run the executable file, run the following command in the same folder as the executable.jar file, which should be the \target subfolder:
 
-mvn clean install tomcat7:exec-war-only
+java -jar executable.jar
 
- */
+The URL to get to the page after the JAR is run is:
+
+http://localhost:8080/playthegame
+
+NOTE: in eclipse the URL will likely be:
+
+http://localhost:8080/numberguesser/playthegame
+
+**/
 @WebServlet(name = "playthegame", urlPatterns = { "/playthegame" })
 public class NumberGuesserGame extends HttpServlet {
 	private static final long serialVersionUID = 1L;
